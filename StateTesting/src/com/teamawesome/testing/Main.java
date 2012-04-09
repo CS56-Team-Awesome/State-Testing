8c4686f8cea58fcdbeaa8914cdb92bbb22320b8f
Hasen Ahmad <kaizokuace@gmail.com>
2012-04-08 21:52:01 -0700

    we did shit

--- StateTesting/src/com/teamawesome/testing/Main.java
+++ StateTesting/src/com/teamawesome/testing/Main.java
@@ -1,6 +1,9 @@
 package com.teamawesome.testing;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -10,7 +13,7 @@ import com.jme3.scene.shape.Box;
 
 /**
  * test
- * @author normenhansen
+ * @author Hasen
  */
 public class Main extends SimpleApplication {
 
@@ -21,14 +24,20 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        
+        inputManager.clearMappings();
+        inputManager.addMapping("Exit", new KeyTrigger(KeyInput.KEY_ESCAPE));inputManager.addMapping("Move Block Down", new KeyTrigger(KeyInput.KEY_DOWN), new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Move Block Up", new KeyTrigger(KeyInput.KEY_UP), new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Move Block Left", new KeyTrigger(KeyInput.KEY_LEFT), new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Move Block Right", new KeyTrigger(KeyInput.KEY_RIGHT), new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Drop Block", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("Pause Game", new KeyTrigger(KeyInput.KEY_P));
+        inputManager.addMapping("Unpause Game", new KeyTrigger(KeyInput.KEY_U));
+     
+        stateManager.attach(new RunningState());
+        stateManager.attach(new PausedState());
+        
+        
     }
 
     @Override
---
+++ StateTesting/src/com/teamawesome/testing/RunningState.java
@@ -0,0 +1,115 @@
+
+package com.teamawesome.testing;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial.CullHint;
+import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
+
+/**
+ *
+ * @author kaizokuace
+ */
+public class RunningState extends AbstractAppState {
+    
+    private SimpleApplication app;
+    private Node              rootNode;
+    private AssetManager      assetManager;
+    private AppStateManager   stateManager;
+    private InputManager      inputManager;
+    private ViewPort          viewPort;
+    
+
+    public RunningState() {
+    }
+    
+    
+    @Override
+    public void cleanup() {
+        super.cleanup();
+    }
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = (SimpleApplication) app; // can cast Application to something more specific
+        this.rootNode     = this.app.getRootNode();
+        this.assetManager = this.app.getAssetManager();
+        this.stateManager = this.app.getStateManager();
+        this.inputManager = this.app.getInputManager();
+        this.viewPort     = this.app.getViewPort();
+        
+        /** A simple textured cube. */ 
+        Box boxshape1 = new Box(Vector3f.ZERO, 1f,1f,1f); 
+        Geometry cube = new Geometry("A Textured Box", boxshape1); 
+        Material mat_stl = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md"); 
+        Texture tex_ml = assetManager.loadTexture("Interface/background.png"); 
+        mat_stl.setTexture("ColorMap", tex_ml); 
+        cube.setMaterial(mat_stl);  
+        rootNode.attachChild(cube);
+        /*------------------------------------*/
+        
+        
+        //cam.setLocation(new Vector3f(0,0,50));
+        stateManager.getState(RunningState.class).setEnabled(true);
+        System.out.println("RunningState Initialized");
+        
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        if(enabled){
+        
+        rootNode.getChild("A Textured Box").setCullHint(CullHint.Inherit);
+     
+        ActionListener actionListener = new ActionListener() {
+            float x = 0;
+            float y = 0;
+            public void onAction(String name, boolean keyPressed, float tpf) {
+                 if ("Pause Game".equals(name) && !keyPressed) {
+                     RunningState.this.stateManager.getState(RunningState.class).setEnabled(false);
+                     RunningState.this.stateManager.getState(PausedState.class).setEnabled(true);
+                     System.out.println("RunningState disabled");
+                     //rootNode.getChild("A Textured Box").setCullHint(CullHint.Always);
+                     inputManager.removeListener(this);
+                     
+             }
+                 if ("Drop Block".equals(name) && !keyPressed) System.out.println("Drop pushed");
+                 if ("Move Block Left".equals(name) && !keyPressed)   rootNode.getChild("A Textured Box").setLocalTranslation(x -= 2.5, y, 0);
+                 if ("Move Block Right".equals(name) && !keyPressed)   rootNode.getChild("A Textured Box").setLocalTranslation(x += 2.5, y, 0);
+                 if ("Move Block Up".equals(name) && !keyPressed)   rootNode.getChild("A Textured Box").setLocalTranslation(x, y += 2.5, 0);
+                 if ("Move Block Down".equals(name) && !keyPressed)   rootNode.getChild("A Textured Box").setLocalTranslation(x, y -= 2.5, 0);
+                 if ("Exit".equals(name) && !keyPressed) app.stop();
+              }
+            };
+        
+        inputManager.addListener(actionListener, new String[]{"Pause Game","Drop Block","Move Block Right", 
+                                                            "Move Block Left", "Move Block Up", "Move Block Down", "Exit"});
+        }
+        else{
+           
+        }
+            
+    }
+
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+    }
+    
+}
