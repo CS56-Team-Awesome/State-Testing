7d0fd0dd26bf85fd3a8aca7a6e005b1ed70cd290
Kayvan Boudai <Kayvan.Boudai@gmail.com>
2012-04-22 01:13:55 -0700

    1

--- StateTesting/src/com/teamawesome/testing/Main.java
+++ StateTesting/src/com/teamawesome/testing/Main.java
@@ -7,11 +7,19 @@ import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 import com.jme3.system.JmeContext;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.builder.LayerBuilder;
+import de.lessvoid.nifty.builder.PanelBuilder;
+import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
+import de.lessvoid.nifty.screen.DefaultScreenController;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.util.Set;
@@ -53,22 +61,27 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         
+        cam.setLocation(new Vector3f(0,0,50));
+        
         inputManager.clearMappings();
-        inputManager.addMapping("Exit", new KeyTrigger(KeyInput.KEY_ESCAPE));inputManager.addMapping("Move Block Down", new KeyTrigger(KeyInput.KEY_DOWN), new KeyTrigger(KeyInput.KEY_S));
+        
+        inputManager.addMapping("Exit", new KeyTrigger(KeyInput.KEY_ESCAPE));
+        inputManager.addMapping("Move Block Down", new KeyTrigger(KeyInput.KEY_DOWN), new KeyTrigger(KeyInput.KEY_S));
         inputManager.addMapping("Move Block Up", new KeyTrigger(KeyInput.KEY_UP), new KeyTrigger(KeyInput.KEY_W));
         inputManager.addMapping("Move Block Left", new KeyTrigger(KeyInput.KEY_LEFT), new KeyTrigger(KeyInput.KEY_A));
         inputManager.addMapping("Move Block Right", new KeyTrigger(KeyInput.KEY_RIGHT), new KeyTrigger(KeyInput.KEY_D));
         inputManager.addMapping("Drop Block", new KeyTrigger(KeyInput.KEY_SPACE));
         inputManager.addMapping("Pause Game", new KeyTrigger(KeyInput.KEY_P));
-        inputManager.addMapping("Unpause Game", new KeyTrigger(KeyInput.KEY_U));
-     
+        
         stateManager.attach(new RunningState());
         stateManager.attach(new PausedState());
         stateManager.getState(PausedState.class).setSettings(settings);
         stateManager.getState(RunningState.class).setCam(cam);
         
-        
     }
+        
+        
+    
 
     @Override
     public void simpleUpdate(float tpf) {
--- StateTesting/src/com/teamawesome/testing/PausedState.java
+++ StateTesting/src/com/teamawesome/testing/PausedState.java
@@ -3,26 +3,31 @@ package com.teamawesome.testing;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
-
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.asset.AssetManager;
-import com.jme3.font.BitmapText;
 import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.KeyTrigger;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial.CullHint;
 import com.jme3.system.AppSettings;
 import com.jme3.ui.Picture;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.builder.LayerBuilder;
+import de.lessvoid.nifty.builder.PanelBuilder;
+import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
+import de.lessvoid.nifty.screen.DefaultScreenController;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 
 /**
  *
  * @author kaizokuace
  */
-public class PausedState extends AbstractAppState {
+public class PausedState extends AbstractAppState implements ScreenController{
     
     private SimpleApplication app;
     private Node              rootNode;
@@ -32,6 +37,9 @@ public class PausedState extends AbstractAppState {
     private ViewPort          viewPort;
     private Node              guiNode;
     private AppSettings       settings;
+    
+    private Nifty nifty;
+    private Screen screen;
 
     public void setSettings(AppSettings s) {
         this.settings = s;
@@ -66,14 +74,58 @@ public class PausedState extends AbstractAppState {
         super.setEnabled(enabled);
         if(enabled){
             
-            guiNode.detachAllChildren();
-            Picture pic = new Picture("HUD Picture");
-            pic.setImage(assetManager, "Interface/background.png", true);
-            pic.setWidth(settings.getWidth()/2);
-            pic.setHeight(settings.getHeight()/2);
-            pic.setPosition(settings.getWidth()/4, settings.getHeight()/4);
-            guiNode.attachChild(pic);
-            System.out.println("PausedState enabled");
+//            guiNode.detachAllChildren();
+//            Picture pic = new Picture("HUD Picture");
+//            pic.setImage(assetManager, "Interface/background.png", true);
+//            pic.setWidth(settings.getWidth()/2);
+//            pic.setHeight(settings.getHeight()/2);
+//            pic.setPosition(settings.getWidth()/4, settings.getHeight()/4);
+//            guiNode.attachChild(pic);
+//            System.out.println("PausedState enabled");
+            
+            
+            
+            NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, app.getAudioRenderer(), app.getGuiViewPort());
+            nifty = niftyDisplay.getNifty();
+            app.getGuiViewPort().addProcessor(niftyDisplay);
+            app.getFlyByCamera().setDragToRotate(true);
+            
+            nifty.loadStyleFile("nifty-default-styles.xml");
+            nifty.loadControlFile("nifty-default-controls.xml");
+ 
+            // <screen>
+            nifty.addScreen("Screen_ID", new ScreenBuilder("Hello Nifty Screen"){{
+             controller(new DefaultScreenController()); // Screen properties       
+ 
+            // <layer>
+            layer(new LayerBuilder("Layer_ID") {{
+                childLayoutVertical(); // layer properties, add more...
+ 
+            // <panel>
+            panel(new PanelBuilder("Panel_ID") {{
+             childLayoutCenter(); // panel properties, add more...               
+ 
+            // GUI elements
+            control(new ButtonBuilder("Button_ID", "Hello Nifty"){{
+              alignCenter();
+                valignCenter();
+                height("5%");
+                width("15%");
+                     }});
+ 
+           //.. add more GUI elements here              
+ 
+             }});
+           // </panel>
+             }});
+           // </layer>
+             }}.build(nifty));
+           // </screen>
+ 
+        nifty.gotoScreen("Screen_ID"); // start the screen
+   
+            
+            
 
         
         ActionListener actionListener = new ActionListener() {
@@ -83,7 +135,6 @@ public class PausedState extends AbstractAppState {
                  if ("Pause Game".equals(name) && !keyPressed) {
                      PausedState.this.stateManager.getState(PausedState.class).setEnabled(false);
                      PausedState.this.stateManager.getState(RunningState.class).setEnabled(true);
-                     guiNode.detachAllChildren();
                      System.out.println("PausedState disabled");
                      inputManager.removeListener(this);
                  
@@ -108,6 +159,21 @@ public class PausedState extends AbstractAppState {
     @Override
     public void update(float tpf) {
         super.update(tpf);
+    }
+
+    //------------Nifty-----------//
+    
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+        this.screen = screen;
+    }
+
+    public void onStartScreen() {
+        
+    }
+
+    public void onEndScreen() {
+        
     }
     
 }
